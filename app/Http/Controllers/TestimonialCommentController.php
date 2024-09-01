e07953f283e172a083284ff7b0d0b1629470917a
ao627515 <ao627515@gmail.com>
2024-09-01 18:50:09 +0000

    php artisan make:model TestimonialComment --all

---
+++ app/Http/Controllers/TestimonialCommentController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreTestimonialCommentRequest;
+use App\Http\Requests\UpdateTestimonialCommentRequest;
+use App\Models\TestimonialComment;
+
+class TestimonialCommentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTestimonialCommentRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(TestimonialComment $testimonialComment)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(TestimonialComment $testimonialComment)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateTestimonialCommentRequest $request, TestimonialComment $testimonialComment)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(TestimonialComment $testimonialComment)
+    {
+        //
+    }
+}
