7834180b1afac742853df56b896cc97da1cde060
ao627515 <ao627515@gmail.com>
2024-09-01 18:47:52 +0000

    php artisan make:model TestimonialFeedback --all

---
+++ app/Http/Controllers/TestimonialFeedbackController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreTestimonialFeedbackRequest;
+use App\Http\Requests\UpdateTestimonialFeedbackRequest;
+use App\Models\TestimonialFeedback;
+
+class TestimonialFeedbackController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTestimonialFeedbackRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(TestimonialFeedback $testimonialFeedback)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(TestimonialFeedback $testimonialFeedback)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateTestimonialFeedbackRequest $request, TestimonialFeedback $testimonialFeedback)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(TestimonialFeedback $testimonialFeedback)
+    {
+        //
+    }
+}
