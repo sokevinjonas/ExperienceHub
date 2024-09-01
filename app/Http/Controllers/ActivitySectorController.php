e37e0231248b201f392ebbec57c705118644ab1e
ao627515 <ao627515@gmail.com>
2024-09-01 18:40:34 +0000

    php artisan make:model ActivitySector --all

---
+++ app/Http/Controllers/ActivitySectorController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreActivitySectorRequest;
+use App\Http\Requests\UpdateActivitySectorRequest;
+use App\Models\ActivitySector;
+
+class ActivitySectorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreActivitySectorRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ActivitySector $activitySector)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ActivitySector $activitySector)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateActivitySectorRequest $request, ActivitySector $activitySector)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ActivitySector $activitySector)
+    {
+        //
+    }
+}
