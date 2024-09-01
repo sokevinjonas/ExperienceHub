bcf365c1ca439ccd6d6812606358116f826a2599
ao627515 <ao627515@gmail.com>
2024-09-01 18:48:37 +0000

    php artisan make:model TestimonialLike --all

---
+++ app/Http/Controllers/TestimonialLikeController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreTestimonialLikeRequest;
+use App\Http\Requests\UpdateTestimonialLikeRequest;
+use App\Models\TestimonialLike;
+
+class TestimonialLikeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTestimonialLikeRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(TestimonialLike $testimonialLike)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(TestimonialLike $testimonialLike)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateTestimonialLikeRequest $request, TestimonialLike $testimonialLike)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(TestimonialLike $testimonialLike)
+    {
+        //
+    }
+}
