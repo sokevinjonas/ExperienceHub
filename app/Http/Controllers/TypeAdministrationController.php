60c0eba9884c7c2baffa95f0b1fcd83dc75ea4af
ao627515 <ao627515@gmail.com>
2024-09-01 18:39:47 +0000

    php artisan make:model TypeAdministration --all

---
+++ app/Http/Controllers/TypeAdministrationController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreTypeAdministrationRequest;
+use App\Http\Requests\UpdateTypeAdministrationRequest;
+use App\Models\TypeAdministration;
+
+class TypeAdministrationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTypeAdministrationRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(TypeAdministration $typeAdministration)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(TypeAdministration $typeAdministration)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateTypeAdministrationRequest $request, TypeAdministration $typeAdministration)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(TypeAdministration $typeAdministration)
+    {
+        //
+    }
+}
