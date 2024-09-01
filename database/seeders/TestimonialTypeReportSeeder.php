bc6b7b3c392e5f4cb0fbf1fc74045a1f54070f55
ao627515 <ao627515@gmail.com>
2024-09-01 18:52:46 +0000

    php artisan make:model TestimonialTypeReport --all

---
+++ database/seeders/TestimonialTypeReportSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TestimonialTypeReportSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
