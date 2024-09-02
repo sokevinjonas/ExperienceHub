c0c5958a8b860c1b3108c3eee2d3c706c86289a4
ao627515 <ao627515@gmail.com>
2024-09-02 07:41:49 +0000

    php artisan make:model TestimonialTypeReport --all

---
+++ app/Http/Controllers/TestimonialTypeReportController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreTestimonialTypeReportRequest;
+use App\Http\Requests\UpdateTestimonialTypeReportRequest;
+use App\Models\TestimonialTypeReport;
+
+class TestimonialTypeReportController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTestimonialTypeReportRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(TestimonialTypeReport $testimonialTypeReport)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(TestimonialTypeReport $testimonialTypeReport)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateTestimonialTypeReportRequest $request, TestimonialTypeReport $testimonialTypeReport)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(TestimonialTypeReport $testimonialTypeReport)
+    {
+        //
+    }
+}
