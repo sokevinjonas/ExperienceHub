8ddfb7df686326e42123df10bd48371e4bd0b09f
ao627515 <ao627515@gmail.com>
2024-09-02 07:39:57 +0000

    php artisan make:model TestimonialDislike --all + php artisan make:model TestimoniaLike --all

---
+++ app/Http/Controllers/TestimonialDislikeController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreTestimonialDislikeRequest;
+use App\Http\Requests\UpdateTestimonialDislikeRequest;
+use App\Models\TestimonialDislike;
+
+class TestimonialDislikeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTestimonialDislikeRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(TestimonialDislike $testimonialDislike)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(TestimonialDislike $testimonialDislike)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateTestimonialDislikeRequest $request, TestimonialDislike $testimonialDislike)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(TestimonialDislike $testimonialDislike)
+    {
+        //
+    }
+}
