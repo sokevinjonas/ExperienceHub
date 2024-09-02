30ace1c876de895fd61ece22dbc2d0e5b8136008
ao627515 <ao627515@gmail.com>
2024-09-02 07:58:07 +0000

    Added 'name' and 'description' columns to 'activity_sectors' table.

--- database/migrations/2024_09_01_184004_create_activity_sectors_table.php
+++ database/migrations/2024_09_01_184004_create_activity_sectors_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('activity_sectors', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->text('description');
             $table->timestamps();
         });
     }
