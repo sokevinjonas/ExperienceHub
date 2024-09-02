849e74bffd30287f0908914b78f7e5513596958f
ao627515 <ao627515@gmail.com>
2024-09-02 07:54:53 +0000

    "Updated User model to use guarded attributes instead of fillable, added role relationship, and modified users table migration to include phone and role_id columns."

--- app/Models/User.php
+++ app/Models/User.php
@@ -16,10 +16,11 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
+    protected $guarded = [
+        'id',
+        'email_verified_at',
+        'created_at',
+        'updated_at'
     ];
 
     /**
@@ -43,5 +44,10 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function role()
+    {
+        return $this->belongsTo(UserRole::class);
     }
 }
--- database/migrations/2024_09_01_183854_create_users_table.php
+++ database/migrations/2024_09_01_183854_create_users_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('phone')->unique();
+            $table->foreignId('role_id')->constrained('user_roles')->onDelete('cascade')->onUpdate('cascade');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
